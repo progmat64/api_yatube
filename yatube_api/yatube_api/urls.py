e97d28d8ee12414c6fbb72e8f714f80d775bd6a2
Александр Матвеев <admatveev17@gmail.com>
2023-03-17 23:00:22 +0300

    Review 4

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,9 +1,9 @@
+from rest_framework.authtoken import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
